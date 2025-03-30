5d83f048c5e77f6f6ed745683e59736d617dca64
trinidadleonard <tleonard@mail.austral.edu.ar>
2025-03-30 17:08:32 -0300

    Update ada.py

--- ada.py
+++ ada.py
@@ -1,3 +1,9 @@
 def ada():
     first_name = "AdA"
     last_name = "LoVeLAce"
+    first_name = "ada"
+    last_name = " lovelace"
+    print(first_name+last_name)
+    print("Ada"+" Lovelace")
+    print("ADA"+" LOVELACE")
+    print(f"\t {first_name}{last_name}")
